360c81339f72b13dff01d59a181c58e79b50702e
Matt <mattcedricybanezzz@gmail.com>
2024-09-30 22:15:41 +0800

    log in

--- laundry_delivery_system/laundry_delivery_system/settings.py
+++ laundry_delivery_system/laundry_delivery_system/settings.py
@@ -115,8 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
+
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
