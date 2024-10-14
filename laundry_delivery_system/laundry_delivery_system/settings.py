47e17eb90603f82884fb2555c9176af7f82d7bb0
Alden Niño N. Cabrera <aldencabrera046@gmail.com>
2024-10-14 16:25:29 +0800

    css test

--- laundry_delivery_system/laundry_delivery_system/settings.py
+++ laundry_delivery_system/laundry_delivery_system/settings.py
@@ -114,11 +114,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 
