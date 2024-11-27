5e6e214c23eb52a6e09af2caaaf4fbfd3c5cf1a6
Alden Niño N. Cabrera <aldencabrera046@gmail.com>
2024-11-27 09:30:27 +0800

    Added requirements.txt

--- IM_Project/laundry_Delivery/users/admin.py
+++ IM_Project/laundry_Delivery/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile, Customer, Driver, Business
+from .models import UserProfile, Customer,Order, Driver, Business
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Business)
+admin.site.register(Order)
